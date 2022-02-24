1157942d5e1e6a76875bf56373c7a3a3b991463b
Ming-der Wang <mingderwang@gmail.com>
2022-02-24 18:26:21 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+const App = () => {
+  return <div>working</div>
+}
+
+const app = document.getElementById('root')
+ReactDOM.render(<App />, app)
