90bdef010a9b254ea8d049ceeebb116956929944
Ming-der Wang <mingderwang@gmail.com>
2022-05-20 12:22:41 +0800

    v0.0.1-apollo-client

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { App } from './App'
+import React from 'react'
+import { render } from 'react-dom'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client'
 
-const app = document.getElementById('root')
-ReactDOM.render(<App />, app)
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+})
+
+function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+    </div>
+  )
+}
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
